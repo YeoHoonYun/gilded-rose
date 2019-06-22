9fc2b58421a3e2e87ab24119b692779ca25077ff
Park Sung chul <oneyedev@gmail.com>
2019-06-22 12:30:04 +0900

    add step1 coverage 100%

---
+++ src/test/java/step1/GildedRoseTest.java
@@ -0,0 +1,270 @@
+package step1;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class GildedRoseTest {
+
+	Item item = new Item("더미", 0, 0);
+	List<Item> items = Arrays.asList(item);
+	GildedRose gildedRose = new GildedRose(items);
+	
+	@Test
+	public void 일반_아이템은_기본적으로_품질이_1씩_떨어집니다() {
+		// given
+		item.name = "일반 아이템";
+		item.quality = 10;
+		item.sellIn = 10;
+		
+		// when
+		gildedRose.updateQuality();
+		
+		// then
+		assertEquals(9, item.getQuality());
+		assertEquals(9, item.getSellIn());
+	}
+	
+	@Test
+	public void 일반_아이템은_판매기한이_지나면_품질이_2씩_떨어집니다() {
+		// given
+		item.name = "일반 아이템";
+		item.quality = 10;
+		item.sellIn = 0;
+		
+		// when
+		gildedRose.updateQuality();
+		
+		// then
+		assertEquals(8, item.getQuality());
+		assertEquals(-1, item.getSellIn());
+	}
+	
+	@Test
+	public void 일반_아이템의_품질은_0보다_낮을_수_없습니다() {
+		// given
+		item.name = "일반 아이템";
+		item.quality = 0;
+		item.sellIn = 10;
+		
+		// when
+		gildedRose.updateQuality();
+		
+		// then
+		assertEquals(0, item.getQuality());
+		assertEquals(9, item.getSellIn());
+	}
+	
+	@Test
+	public void 일반_아이템의_품질은_판매기한이_지나도_0보다_낮을_수_없습니다() {
+		// given
+		item.name = "일반 아이템";
+		item.quality = 1;
+		item.sellIn = 0;
+		
+		// when
+		gildedRose.updateQuality();
+		
+		// then
+		assertEquals(0, item.getQuality());
+		assertEquals(-1, item.getSellIn());
+	}
+	
+	@Test
+	public void Aged_Brie_아이템의_품질은_도리어_1씩_증가합니다() {
+		// given
+		item.name = "Aged Brie";
+		item.quality = 10;
+		item.sellIn = 10;
+		
+		// when
+		gildedRose.updateQuality();
+		
+		// then
+		assertEquals(11, item.getQuality());
+		assertEquals(9, item.getSellIn());
+	}
+	
+	@Test
+	public void Aged_Brie_아이템의_품질은_50을_넘을_수_없습니다() {
+		// given
+		item.name = "Aged Brie";
+		item.quality = 50;
+		item.sellIn = 10;
+		
+		// when
+		gildedRose.updateQuality();
+		
+		// then
+		assertEquals(50, item.getQuality());
+		assertEquals(9, item.getSellIn());
+	}
+	
+	@Test
+	public void Aged_Brie_아이템의_품질은_판매기간이_지나면_2씩_증가합니다() {
+		// given
+		item.name = "Aged Brie";
+		item.quality = 10;
+		item.sellIn = 0;
+		
+		// when
+		gildedRose.updateQuality();
+		
+		// then
+		assertEquals(12, item.getQuality());
+		assertEquals(-1, item.getSellIn());
+	}
+	
+	@Test
+	public void Aged_Brie_아이템의_품질은_판매기간이_지나도_50을_넘을_수_없습니다() {
+		// given
+		item.name = "Aged Brie";
+		item.quality = 50;
+		item.sellIn = 0;
+		
+		// when
+		gildedRose.updateQuality();
+		
+		// then
+		assertEquals(50, item.getQuality());
+		assertEquals(-1, item.getSellIn());
+	}
+	
+	@Test
+	public void Sulfuras_아이템은_품질과_판매기한_모두_절대_변하지_않습니다() {
+		// given
+		item.name = "Sulfuras, Hand of Ragnaros";
+		item.quality = 10;
+		item.sellIn = 10;
+		
+		// when
+		gildedRose.updateQuality();
+		
+		// then
+		assertEquals(10, item.getQuality());
+		assertEquals(10, item.getSellIn());
+	}
+	
+	@Test
+	public void Sulfuras_아이템의_품질은_0에서_50제한과_상관없이_변하지_않습니다() {
+		// given
+		item.name = "Sulfuras, Hand of Ragnaros";
+		item.quality = 80;
+		item.sellIn = -1;
+		
+		// when
+		gildedRose.updateQuality();
+		
+		// then
+		assertEquals(80, item.getQuality());
+		assertEquals(-1, item.getSellIn());
+	}
+	
+	@Test
+	public void Backstage_아이템의_품질은_판매기한이_10일보다_많이_남으면_1씩_증가합니다() {
+		// given
+		item.name = "Backstage passes to a TAFKAL80ETC concert";
+		item.quality = 10;
+		item.sellIn = 11;
+		
+		// when
+		gildedRose.updateQuality();
+		
+		// then
+		assertEquals(11, item.getQuality());
+		assertEquals(10, item.getSellIn());
+	}
+	
+	@Test
+	public void Backstage_아이템의_품질은_판매기한이_10일보다_많아도_50을_넘을_수_없습니다() {
+		// given
+		item.name = "Backstage passes to a TAFKAL80ETC concert";
+		item.quality = 50;
+		item.sellIn = 11;
+		
+		// when
+		gildedRose.updateQuality();
+		
+		// then
+		assertEquals(50, item.getQuality());
+		assertEquals(10, item.getSellIn());
+	}
+	
+	@Test
+	public void Backstage_아이템의_품질은_판매기한이_5일_초과_10일_이하로_남으면_2씩_증가합니다() {
+		// given
+		item.name = "Backstage passes to a TAFKAL80ETC concert";
+		item.quality = 10;
+		item.sellIn = 10;
+		
+		// when
+		gildedRose.updateQuality();
+		
+		// then
+		assertEquals(12, item.getQuality());
+		assertEquals(9, item.getSellIn());
+	}
+	
+	@Test
+	public void Backstage_아이템의_품질은_판매기한이_5일_초과_10일_이하로_남아도_50을_넘을_수_없습니다() {
+		// given
+		item.name = "Backstage passes to a TAFKAL80ETC concert";
+		item.quality = 49;
+		item.sellIn = 10;
+		
+		// when
+		gildedRose.updateQuality();
+		
+		// then
+		assertEquals(50, item.getQuality());
+		assertEquals(9, item.getSellIn());
+	}
+	
+	
+	@Test
+	public void Backstage_아이템의_품질은_판매기한이_1일_이상_5일_이하로_남으면_3씩_증가합니다() {
+		// given
+		item.name = "Backstage passes to a TAFKAL80ETC concert";
+		item.quality = 10;
+		item.sellIn = 5;
+		
+		// when
+		gildedRose.updateQuality();
+		
+		// then
+		assertEquals(13, item.getQuality());
+		assertEquals(4, item.getSellIn());
+	}
+	
+	@Test
+	public void Backstage_아이템의_품질은_판매기한이_1일_이상_5일_이하로_남아도_품질이_50을_넘을_수는_없습니다() {
+		// given
+		item.name = "Backstage passes to a TAFKAL80ETC concert";
+		item.quality = 48;
+		item.sellIn = 5;
+		
+		// when
+		gildedRose.updateQuality();
+		
+		// then
+		assertEquals(50, item.getQuality());
+		assertEquals(4, item.getSellIn());
+	}
+	
+	@Test
+	public void Backstage_아이템의_품질은_판매기한이_지나면_0이_됩니다() {
+		// given
+		item.name = "Backstage passes to a TAFKAL80ETC concert";
+		item.quality = 10;
+		item.sellIn = 0;
+		
+		// when
+		gildedRose.updateQuality();
+		
+		// then
+		assertEquals(0, item.getQuality());
+		assertEquals(-1, item.getSellIn());
+	}
+}
